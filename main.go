0d39ced2e523894e1eeeb7c16508db5268cc961b
takumakume <takuma.kume@pepabo.com>
2018-04-25 14:24:07 +0900

    takumakumed

--- main.go
+++ main.go
@@ -2,37 +2,66 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"time"
 
 	"github.com/takama/daemon"
 )
 
-func main() {
+const (
+	// name of the service
+	name        = "takumakumed"
+	description = "daemon of takumakume"
+)
 
-	service, err := daemon.New("takumakumed", "takumakumemon desu")
-	usage := "Usage: takumakumed install | remove | start | stop | status"
+type Service struct {
+	daemon.Daemon
+}
 
-	if err != nil {
-		fmt.Println("Error: ", err)
-		os.Exit(1)
-	}
-
+func (service *Service) Runner() (string, error) {
 	if len(os.Args) > 1 {
 		command := os.Args[1]
 		switch command {
 		case "install":
-			service.Install()
+			return service.Install()
 		case "remove":
-			service.Remove()
+			return service.Remove()
 		case "start":
-			service.Start()
+			return service.Start()
 		case "stop":
-			service.Stop()
+			return service.Stop()
 		case "status":
-			service.Status()
+			return service.Status()
 		default:
-			fmt.Println(usage)
+			return fmt.Sprintf("Usage: %v [install | remove | start | stop | status]", os.Args[0]), nil
 		}
 	}
 
+	for {
+		Run()
+	}
+
+	return "", nil
+}
+
+func Run() {
+	fmt.Println("hoge")
+	time.Sleep(3 * time.Second)
+}
+
+func main() {
+	d, err = daemon.New(name, description)
+	if err != nil {
+		log.Println("error: ", err)
+		os.Exit(1)
+	}
+
+	service := &Service{d}
+	status, err := service.Runner()
+	if err != nil {
+		log.Println(status, "\nError:", err)
+		os.Exit(1)
+	}
+	fmt.Println(status)
 }
